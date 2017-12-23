eb8e55093081bd16fc00be97b9301ff975b4f2f9
shige0409 <basketball0316pf@gmail.com>
2017-12-23 23:27:24 +0900

    add material design to top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,18 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require bootstrap
 //= require turbolinks
-//= require_tree .
+//= require_tree
+jQuery(function($) {
+    $('.bg-slider').bgSwitcher({
+        images: ['home_images/green.jpg','home_images/blue.jpg','home_images/asphalt.jpg','home_images/orange.jpg', 'home_images/red.jpg'],
+        interval: 4000, // 背景画像を切り替える間隔を指定 3000=3秒
+        loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
+        shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
+        effect: "drop", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
+        duration: 800, // エフェクトの時間を指定します。
+        easing: "swing" // エフェクトのイージングをlinear,swingから指定 // 切り替える背景画像を指定
+    });
+});
