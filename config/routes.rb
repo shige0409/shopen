568f85542e2d0947a68034ce20fb9346dc20b4ca
shige0409 <basketball0316pf@gmail.com>
2018-01-31 18:19:21 +0900

    change application.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#top' #=> root_path
- #get '/' => 'home#top'と同じ
   get '/about', to: 'home#about'
-
+  #resources :users
 end
