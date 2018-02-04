9d0ba9a46736a5ae86580914cca8b27f7346aef9
shige0409 <basketball0316pf@gmail.com>
2018-02-04 22:15:00 +0900

    arrange layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'home#top' #=> root_path
+  get '/test', to: 'home#test'
   get '/about', to: 'home#about'
   get '/contact', to: 'home#contact'
-  # get '/test', to: 'home#test'
   get '/signup', to: 'shops#new'
   post '/signup', to: 'shops#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :shops, except: [:new, :create]
 end
